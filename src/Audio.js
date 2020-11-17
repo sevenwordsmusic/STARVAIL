b0fd3243717d78e92d92743c6be98085c53d6c58
SevenWordsMusic <sevenwordsmusic@gmail.com>
2020-11-17 14:24:53 +0100

    AUDIO_0004V

--- src/Audio.js
+++ src/Audio.js
@@ -742,7 +742,7 @@ export default class Audio extends Phaser.Scene {
     //CREATION:
     create() {
         if(navigator.userAgent.indexOf("Chrome") != -1 ) {
-            console.log("%c | AUDIO ENGINE | %c > GOOGLE CHROME DETECTED : max sound slots reduced, for a better sound experience switch to Firefox.", Audio.ctf, "");
+            console.log("%c | AUDIO ENGINE | %c > GOOGLE CHROME DETECTED : max sound slots reduced; for a better sound experience switch to Firefox.", Audio.ctf, "");
             Audio.maxSFXinstances = 8;
         }
         //INIT AUDIO
