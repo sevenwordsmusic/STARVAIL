267e39a31c085eec1d8a0479f087dfb90e40cc8a
SevenWordsMusic <sevenwordsmusic@gmail.com>
2020-11-17 15:27:51 +0100

    AUDIO_0004X

--- src/Audio.js
+++ src/Audio.js
@@ -29,7 +29,7 @@ export default class Audio extends Phaser.Scene {
     static oldVolumes = [];
     //INSTANCES
     static load;
-    static maxSFXinstances = 8;
+    static maxSFXinstances = 16;
     static SFXinstance = 0;
     static ambientLoop;
     //STINGERS
