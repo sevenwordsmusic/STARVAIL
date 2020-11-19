68982d4582962c6892c5efaa52783064076ba06b
SevenWordsMusic <sevenwordsmusic@gmail.com>
2020-11-19 23:04:42 +0100

    dale

--- src/NPCs/BossAfter.js
+++ src/NPCs/BossAfter.js
@@ -13,8 +13,8 @@ export default class BossAfter extends FiniteStateMachine{
         //AUDIO
 
 
-          Audio.play3DinstanceRnd(this.scene, 59);
-          Audio.play3DinstanceRnd(this.scene, 65);
+          Audio.play2DinstanceRnd(59);
+          Audio.play2DinstanceRnd(65);
           //
 
 
